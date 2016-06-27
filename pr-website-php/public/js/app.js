2c08de12b2a50a43075c3d57fc9a8d30a153fed0
Joel Corporan <joel.corporangomez@upr.edu>
2016-06-27 16:59:41 -0400

    test with video.js

--- pr-website-php/public/js/app.js
+++ pr-website-php/public/js/app.js
@@ -2,21 +2,25 @@
 $(document).ready(function() {
 
     // Player Key
-    jwplayer.key = "d6gsi297N6N7krcFM23KX4G7XqOJJHfHfo7L/g==";
+    //jwplayer.key = "d6gsi297N6N7krcFM23KX4G7XqOJJHfHfo7L/g==";
 
     // Listen to clicks on the thumbnails to start video playback
     $(document).on('click','.thumb-container', function(e){
         var assetid = $(this).children("img").data('assetid');
         $("#videostage").addClass("active");
         $("#videostagectrl").addClass("active");
+        //var playerInstance = videojs("html5Player");
+        //playerInstance.show();
         loadFile(assetid);
     })
 
     $("#videostagectrl").on("click", function(){
         $(this).removeClass('active');
-//        $('#videostage').remove();
-        var playerInstance = jwplayer("videoplayer");
-        playerInstance.remove();
+        
+        var playerInstance = videojs("html5Player");
+        playerInstance.hide();        
+        //var playerInstance = jwplayer("videoplayer");
+        //playerInstance.remove();
         $("#videostage").removeClass("active");
 
     });
@@ -45,13 +49,38 @@ $(document).ready(function() {
     }
 
     function playFile(url){
-        var videoURL = "http://localhost:3000/proxy.m3u8?id=" + url;
 
-        var player = videojs('videoplayer', {
+        var videoURL = "http://localhost:3000/proxy.m3u8?id=" + url;
+        var obj,source;
+
+        obj = document.createElement('video');
+        $(obj).attr('id', 'example_video_test');
+        $(obj).attr('class', 'video-js vjs-default-skin');
+        $(obj).attr('width', '960');
+        $(obj).attr('data-height', '540');
+        $(obj).attr('controls', ' ');
+        $(obj).attr('preload', 'auto');
+        $(obj).attr('data-setup', '{}');
+
+        source = document.createElement('source');
+        $(source).attr('type', 'application/x-mpegURL');
+        $(source).attr('src', videoURL);
+
+        $("#videoplayer").append(obj);
+        $(obj).append(source);
+
+       /* var player = videojs('html5Player', {
+            controls: true,
             autoplay: false,
-            
+            width: 960,
+            height: 540,
+            preload: "off"
         });
 
+        var videoURL = "http://localhost:3000/proxy.m3u8?id=" + url;
+         $("source").attr("src", videoURL);*/
+
+/*
         var playerInstance = jwplayer("videoplayer");
         playerInstance.setup({
             primary: "html5",
@@ -65,6 +94,6 @@ $(document).ready(function() {
         playerInstance.on('error', function(e) {
             console.log(e);
             showError();
-        });
+        });*/
     }
 })
